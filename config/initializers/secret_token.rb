1bbb57fc3570bc1e2fefa255845021946dd28276
Asa Julian <asa.julian@gmail.com>
2013-03-25 12:03:05 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SouthernBlog::Application.config.secret_token = '91d5307915c54104bdc74997fda20ba15f7c4ab236c291d226e999675bd133eb81d8e1ef771392e53f1c152b139bb579ce49509216f5d06bc95c576ab7e9a16c'
