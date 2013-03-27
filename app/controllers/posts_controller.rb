bd18a00c9d03f1c95c3fd9be246766aaf5c0a039
Asa Julian <asa.julian@gmail.com>
2013-03-27 11:13:19 -0400

    Changed posts per page to 10

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   # GET /posts.json
   def index
     #@posts = Post.all
-    @posts = Post.paginate(page: params[:page], :per_page => 3)
+    @posts = Post.paginate(page: params[:page], :per_page => 10)
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @posts }
